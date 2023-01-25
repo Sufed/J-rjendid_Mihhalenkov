f8c7e94c093d17bbe414b82fffa3685bdd85f27e
opilane <opilane@HP-CZC2349HTB>
2023-01-25 14:13:43 +0200

    Add project files.

---
+++ Järjendid_Mihhalenkov.py
@@ -0,0 +1,34 @@
+﻿spisok=[] #Пустой список.
+numbers=[1,2,3,4,5]
+abc=['A','B','C']
+slovo="Programmerimine"
+slovo_list=list(slovo)
+print(slovo)
+print(slovo_list)
+
+while True:
+    print("1-добавить букву в список")
+    print("2-склеить списки\n3-добавить букву на i -позицию")
+    print("4-удалить элемент")
+    valik=int(input())
+    if valik==1:
+        a=input("Введи букву: ")
+        slovo_list.append(a)
+        print(f"Добавили {a} новый список",slovo_list)
+    elif valik==2:
+        slovo_list.extend(abc)
+        print(slovo_list)
+    elif valik==3:
+        a=input("Введи букву, которую хочешь добавить: ")
+        i=input("Введи номер позиции, куда хочешь добавить букву: ")
+        slovo.list.insert(i-1,a) #0,1,2...
+        print(slovo_list)
+    elif valik==4:
+        a=input("Введи букву, которую хочешь удалить: ")
+        n=slovo_list.count(a)
+        if n>0:
+            for i in range(n):
+                slovo_list.remove(a)
+        else:
+            print("Искомой буквы нет")
+        print(slovo_list)
